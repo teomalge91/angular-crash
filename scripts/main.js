195511ec7e2799e79f7c0f01bcd726d777403936
Matteo Malgesini <Matteo@Air-di-Matteo.lan>
2019-07-30 23:13:53 +0200

    cambiate le colonne, aggiunto bottone go up e bottone scroll down

--- scripts/main.js
+++ scripts/main.js
@@ -18,3 +18,27 @@ var timing = setInterval(
             //and you see "It's over" instead of time left
         }
     }, 1000);
+
+window.onscroll = function () { scrollFunction() };
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("top-button").style.display = "block";
+    } else {
+        document.getElementById("top-button").style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+//scroll down
+$(function() {
+    $('a[href*="#"]').on('click', function(e) {
+      e.preventDefault();
+      $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 500, 'linear');
+    });
+  });
