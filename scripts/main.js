3c08780c1f59ce404e0d834ed30621d0feaa5a97
Matteo Malgesini <Matteo@Air-di-Matteo.lan>
2019-09-21 16:20:18 +0200

    vh

--- scripts/main.js
+++ scripts/main.js
@@ -3,14 +3,11 @@ let vh = window.innerHeight * 0.01;
 // Then we set the value in the --vh custom property to the root of the document
 document.documentElement.style.setProperty('--vh', `${vh}px`);
 
-// We listen to the resize event
-if (window.DeviceOrientationEvent) {
-	window.addEventListener("deviceorientation", () => {
-        alert('ciao');
-        let vh = window.innerHeight * 0.01;
-        document.documentElement.style.setProperty('--vh', `${vh}px`);
-    });
-}
+// Listen for orientation changes
+window.addEventListener("orientationchange", function() {
+	let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+}, false);
 
 //funzione che nasconde il loader quando la pagina è completamente caricata
 $(window).on("load", function (e) {
