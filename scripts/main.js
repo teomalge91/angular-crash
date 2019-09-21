20eb65b28e6b4302fddca45b543d66428eff5f4c
Matteo Malgesini <Matteo@Air-di-Matteo.lan>
2019-09-21 14:28:47 +0200

    vh

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,15 @@
+// First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+let vh = window.innerHeight * 0.01;
+// Then we set the value in the --vh custom property to the root of the document
+document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+// We listen to the resize event
+window.addEventListener('resize', () => {
+    // We execute the same script as before
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+  });
+
 //funzione che nasconde il loader quando la pagina è completamente caricata
 $(window).on("load", function (e) {
     // Animate loader off screen
