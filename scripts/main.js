5ec2baa2fc5e4682e4086091793efa5c9be1b5ee
Matteo Malgesini <matteo@Air-di-Matteo.lan>
2020-06-12 12:29:38 +0200

    varie

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,8 @@
+$(document).ready(function () {
+    AOS.init({
+        easing: 'ease-in-out-sine'
+  });
+});
 // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
 let vh = window.innerHeight * 0.01;
 // Then we set the value in the --vh custom property to the root of the document
